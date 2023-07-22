3bd110ef80d5fdb61aac56d2a3d7428383ddea6d
Aleksey <124733308+G00d1n1@users.noreply.github.com>
2023-07-22 10:50:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 import btnCheck from "./module/btnCheck.js";
 import cosmos from "./module/cosmos.js";
 import fishing from "./module/fishing.js";
-import snowAndCosmic from "./module/snowAndCosmic.js"
+import snowMode from "./module/snowMode.js"
 import qube3d from "./module/qube3d.js";
+import cosmos3d from "./module/cosmos3d.js";
+import matrix from "./module/matrix.js";
+import ball from "./module/ball.js";
+import gfttr from "./module/gameFlyToTheRight.js";
+
 
 btnCheck()
 
 fishing()
 
-snowAndCosmic()
+snowMode()
 
 cosmos()
 
-qube3d()
+// qube3d()
+
+// cosmos3d()
+
+// matrix()
+
+ball()
+
+gfttr()
+
